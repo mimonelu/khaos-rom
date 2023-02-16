35aa18a8a8fb761c861d5f986816fe885e9531f9
mimonelu <mimonelu@gmail.com>
2023-02-17 02:31:04 +0900

    update

--- docs/app/immutable/chunks/0-f42bb360.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-9d5734c9.js";export{t as component};
---
+++ docs/app/immutable/components/pages/_page.svelte-db61fafc.js
@@ -0,0 +1,1 @@
+import{S as s,i as n,s as t,o as a}from"../../chunks/index-f3a1764f.js";function l(e){return a(()=>{console.log("[O] Hello!")}),[]}class u extends s{constructor(o){super(),n(this,o,l,null,t,{})}}export{u as default};
